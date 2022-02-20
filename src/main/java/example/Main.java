5cff5d84769bc0e2704293d2fdba2e1e6c13426c
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2022-02-20 12:54:06 +0900

    add to handle MY_SECRET environment variable

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -5,19 +5,22 @@ import com.google.common.io.CharStreams;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
 
-    public static void main(String[] args) throws URISyntaxException, IOException {
+    public static void main(String[] args) throws IOException {
         try (Reader reader = new InputStreamReader(
                 Main.class.getResourceAsStream("/world"), StandardCharsets.UTF_8)) {
             String world = CharStreams.toString(reader);
             System.out.println("Hello " + world);
+
             for (var i = 0; i < args.length; i++) {
                 System.out.println(String.format("args[%d]: %s", i, args[i]));
             }
+
+            String mySecret = System.getenv("MY_SECRET");
+            System.out.println(String.format("your secret is %s", mySecret));
         }
     }
 }
